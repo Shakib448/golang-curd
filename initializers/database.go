aaae7036ad7ff57d684da546dbb4c56efb8e7a80
Shakib448 <shakiba448@gmail.com>
2023-06-14 13:01:55 +0600

    WIP on go curd

--- initializers/database.go
+++ initializers/database.go
@@ -1,5 +1,27 @@
 package initializers
 
-func Database() {
+import (
+	"fmt"
+	"log"
+	"os"
 
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func ConnectToDB() {
+
+	var err error
+	DB, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  os.Getenv("DB_URL"),
+		PreferSimpleProtocol: true,
+	}), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("Database connection failed!")
+	}
+
+	fmt.Println(DB)
 }
